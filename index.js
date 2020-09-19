593584f991b4893f312f6e23a98afe9238307af7
shasharoman <shasharoman@gmail.com>
2020-09-19 20:38:03 +0800

    feat: method about pull terminal colors

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ exports.cp = require('./lib/cp');
 exports.req = require('./lib/req');
 
 exports.sleep = (ms = 100) => new Promise(resolve => setTimeout(resolve, ms));
+// eslint-disable-next-line
+exports.pullCollor = s => s.replace(/\u001b\[(?:\d*;){0,5}\d*m/g, '');
 
 exports.camelCaseKey = camelSnake.camelCaseKey;
 exports.snakeCaseKey = camelSnake.snakeCaseKey;
